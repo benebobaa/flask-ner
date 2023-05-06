18c346c7dc2a375d717c5311070bb4ccec67f508
Benediktus Satriya <93780305+benebobaa@users.noreply.github.com>
2023-05-07 00:24:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -41,4 +41,7 @@ def handle_client_event(data):
     
 from controller import predict_controller
 
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
 
